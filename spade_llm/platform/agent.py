17502c20ddab4df9bd15c7abcd6eb013e27fc0b7
Dmitry Bugaychenko <dmitrybugaychenko@gmail.com>
2025-03-30 01:41:04 +0300

    feat: Introduce Behaviour abstraction and PerformativeDispatcher

--- spade_llm/platform/agent.py
+++ spade_llm/platform/agent.py
@@ -1,11 +1,14 @@
 import uuid
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from typing import Callable, Optional
 
-from spade_llm.platform.api import MessageHandler, Message
+from spade_llm.platform.api import MessageHandler, Message, AgentContext
 
 
 class MessageTemplate:
+    """
+    Templates are used to filter messages and dispatch them to proper behaviour
+    """
     def __init__(self, 
                  thread_id: Optional[uuid.UUID] = None, 
                  performative: Optional[str] = None,
@@ -56,4 +59,54 @@ class MessageTemplate:
 
 
 class Behaviour(MessageHandler, metaclass=ABCMeta):
-    pass
+    """
+    Reusable code block for the agent, consumes a message matching certain template and
+    handles it
+    """
+    @property
+    def template(self) -> MessageTemplate:
+        pass
+
+    @abstractmethod
+    def is_done(self) -> bool:
+        """
+        Returns true if behaviour is completed and should not accept messages any more and false otherwise
+        """
+        pass
+
+class PerformativeDispatcher(MessageHandler):
+    """
+    Stores behaviours grouped by performative configured in their template. Uses dictionary with performative
+    as a key and list of behaviours as values plus extra list for behaviours without performative specified
+    """
+
+    def add_behaviour(self, beh: Behaviour):
+        """
+        Add behaviour to dispatch list
+        :param beh: Behaviour to add.
+        """
+        pass
+
+    def remove_behaviour(self, beh: Behaviour):
+        """
+        Removes behaviour from dispatch
+        :param beh: Behaviour to remove.
+        """
+        pass
+
+    @property
+    def is_empty(self) -> bool:
+        """Returns true if there are no behaviours"""
+        pass
+
+    def find_matching_behaviour(self, msg: Message) -> Optional[Behaviour]:
+        """Lookups for behaviour matching given message, first select proper list based on performative,
+        then find the first one with matching template"""
+        pass
+
+    async def handle_message(self, context: AgentContext, message: Message):
+        """
+        Tries to find behaviour matching message and pass message to it. After that check is behaviour is
+        done and if so removes it.
+        """
+        pass
