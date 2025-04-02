004658c68d7f868f30125d55e5e13660b46506d0
Dmitry Bugaychenko <dmitrybugaychenko@gmail.com>
2025-04-03 01:04:07 +0300

    feat: Add new configurations and enhancements to GigaChatModelConfig

--- spade_llm/platform/gigachat.py
+++ spade_llm/platform/gigachat.py
@@ -1,3 +1,5 @@
+import ssl
+
 from spade_llm.platform.conf import configuration, Configurable
 from spade_llm.platform.models import ChatModelFactory, ChatModelConfiguration
 from pydantic import Field
@@ -18,6 +20,50 @@ class GigaChatModelConfig(ChatModelConfiguration):
     logit_bias: Optional[dict] = Field(default={}, description="Modify the likelihood of specified tokens appearing in the completion.")
     user: Optional[str] = Field(default="", description="A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.")
 
+    base_url: Optional[str] = None
+    """ Base API URL """
+    auth_url: Optional[str] = None
+    """ Auth URL """
+    credentials: Optional[str] = None
+    """ Auth Token """
+    scope: Optional[str] = None
+    """ Permission scope for access token """
+
+    access_token: Optional[str] = None
+    """ Access token for GigaChat """
+
+    model: Optional[str] = None
+    """Model name to use."""
+
+    password: Optional[str] = None
+    """ Password for authenticate """
+
+    timeout: Optional[float] = None
+    """ Timeout for request """
+    verify_ssl_certs: Optional[bool] = None
+    """ Check certificates for all requests """
+
+    ca_bundle_file: Optional[str] = None
+    cert_file: Optional[str] = None
+    key_file: Optional[str] = None
+    key_file_password: Optional[str] = None
+
+    profanity: bool = True
+    """ DEPRECATED: Check for profanity """
+    profanity_check: Optional[bool] = None
+    """ Check for profanity """
+    streaming: bool = False
+    """ Whether to stream the results or not. """
+    use_api_for_tokens: bool = False
+    """ Use GigaChat API for tokens count """
+    verbose: bool = False
+    """ Verbose logging """
+    flags: Optional[List[str]] = None
+    """ Feature flags """
+    repetition_penalty: Optional[float] = None
+    """ The penalty applied to repeated tokens """
+    update_interval: Optional[float] = None
+    """ Minimum interval in seconds that elapses between sending tokens """
 
 @configuration(GigaChatModelConfig)
 class GigaChatModelFactory(ChatModelFactory[GigaChat], Configurable[GigaChatModelConfig]):
