93206420e04ebb5d6fd001cda9531353414d7f59
Dmitry Bugaychenko <dmitrybugaychenko@gmail.com>
2025-04-02 23:31:54 +0300

    test: Add unit tests for configurable parsing

---
+++ tests/test_config.py
@@ -0,0 +1,50 @@
+import asyncio
+import unittest
+
+from pydantic import BaseModel, Field
+
+from spade_llm.platform.behaviors import (
+    Behaviour,
+    BehaviorsOwner,
+    MessageTemplate,
+    Message,
+    MessageHandlingBehavior,
+)
+from spade_llm.platform.api import AgentId
+from spade_llm.platform.conf import Configurable, configuration, ConfigurableRecord
+
+
+class StringConfig(BaseModel):
+    s: str = Field(description="Single field configuration for testing")
+
+@configuration(StringConfig)
+class StringConfigurable(Configurable[StringConfig]):
+    pass
+
+class IntConfig(BaseModel):
+    i: int = Field(description="Single int field configuration for testing")
+
+class MultipleConfig(StringConfig, IntConfig):
+    pass
+
+class TestConfig(unittest.TestCase):
+
+
+    def test_configurable_parse_config(self):
+        expected = StringConfig(s="str")
+
+        conf = StringConfigurable.parse_config(expected.model_dump_json())
+
+        self.assertEqual(expected, conf)
+
+    def test_configurable_load_string_config(self):
+        conf = ConfigurableRecord.model_validate_json('{"type_name":"tests.test_config.StringConfigurable", "s":"str"}')
+
+        parsed = conf.create_instance()
+
+        self.assertEqual("str", parsed.config().s)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
